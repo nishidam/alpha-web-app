b37535d1b1c4f3d461dc953b89fbb507b4958f2b
nishidam <Makoto.Nishida@sony.com>
2018-09-30 14:41:41 +0900

    home画面を切り分け

--- src/Main.js
+++ src/Main.js
@@ -17,12 +17,12 @@ class Main extends Component {
                 <div>
                     <h1>Simple SPA</h1>
                     <ul className="header">
-                        <li><NavLink exact to="/">Home</NavLink></li>
+                        <li><NavLink to="/Home">Home</NavLink></li>
                         <li><NavLink to="/stuff">Stuff</NavLink></li>
                         <li><NavLink to="/contact">Contact</NavLink></li>
                     </ul>
                     <div className="content">
-                        <Route exaxt path="/" component={Home} />
+                        <Route path="/Home" component={Home} />
                         <Route path="/stuff" component={Stuff} />
                         <Route path="/contact" component={Contact} />
                     </div>
