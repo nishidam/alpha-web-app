85dfe305dddbabd6aa1057a1275ae40831b8727d
nishidam <Makoto.Nishida@sony.com>
2018-09-30 14:30:09 +0900

    alpha-web-appの作成

---
+++ src/Main.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+import {
+    Route,
+    NavLink,
+    HashRouter
+} from "react-router-dom";
+import Home from "./contents/Home";
+import Stuff from "./contents/Stuff";
+import Contact from "./contents/Contact";
+
+
+class Main extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <h1>Simple SPA</h1>
+                    <ul className="header">
+                        <li><NavLink exact to="/">Home</NavLink></li>
+                        <li><NavLink to="/stuff">Stuff</NavLink></li>
+                        <li><NavLink to="/contact">Contact</NavLink></li>
+                    </ul>
+                    <div className="content">
+                        <Route exaxt path="/" component={Home} />
+                        <Route path="/stuff" component={Stuff} />
+                        <Route path="/contact" component={Contact} />
+                    </div>
+                </div>
+            </HashRouter>
+        );
+    }
+}
+
+export default Main;
